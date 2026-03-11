2b22e8bffd39fadedae6becb4e8a9f8999057012
AnthonyGuardado <mg25001@itcha.edu.sv>
2026-03-11 09:13:20 -0600

    Agregamos tailwind y PrimeVue

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,30 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import Button from 'primevue/button'
+import Card from 'primevue/card'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <div class="min-h-screen bg-slate-900 flex items-center justify-center p-10">
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+    <Card class="w-full max-w-md shadow-2xl">
+      <template #title>
+        <div class="text-center text-2xl font-bold">
+          🚀 TechStore UI Test
+        </div>
+      </template>
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
+      <template #content>
+        <p class="mb-6 text-center">
+          Si ves esto con estilo moderno,
+          PrimeVue y Tailwind están funcionando correctamente.
+        </p>
 
-  <RouterView />
+        <div class="flex justify-center gap-4">
+          <Button label="Primario" icon="pi pi-check" />
+          <Button label="Secundario" severity="secondary" />
+        </div>
+      </template>
+    </Card>
+
+  </div>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
